a17e60c5c50fe13fda3d83b84f680a372311784b
aleks_3vs4n4y <aleksi.alatalo@gmail.com>
2024-01-21 16:07:17 +0200

    Change tutorial gif and reword howto pt.2

--- src/app.tsx
+++ src/app.tsx
@@ -7,26 +7,29 @@ import '../src/assets/style.css';
 const App: React.FC = () => {
     return (
         <div className="grid wrapper">
-            <div className="app-card">
-                <p className="app-card--description p-medium">
-                    Miro Birdseye is an extension that helps you declutter your workspace and make professional presentations by hiding information not currently necessary.
-                </p>
-            </div>
 
 
             <div className="cs1 ce12">
-                <img src="/src/assets/tutorialGIf.gif" alt="Tutorial gif" />
+            <em>Miro Birdseye is an extension that helps you declutter your workspace and make professional presentations by hiding information not currently necessary.
+            </em>
+                </div>
+
+
+
+
+            <div className="cs1 ce12">
+                <img src="/src/assets/tutorialGIf.gif" alt="Tutorial gif"/>
             </div>
+
 
             <div className="cs1 ce12">
                 <h1>HowTOs:</h1>
-                <p>1: Create a frame and highlight it.</p>
-                <p>2: Press the "Create a curtain" button.</p>
+                <p>1: Create a frame or highlight the nodes you want to group.</p>
+                <p>2: Press the "Create a curtain" button on the highlighted nodes.</p>
                 <p>3: You just created your first Birdseye curtain!</p>
                 <p>4: Go and create visual and intuitive presentations or declutter you board.</p>
 
             </div>
-
 
         </div>
 
